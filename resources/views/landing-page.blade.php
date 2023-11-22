3e2e5c7a866f895bc4a7d365a0e787efd3b59350
yandi <lintangyandinugraha@gmail.com>
2023-11-22 18:54:04 +0700

    Fix responsive for home/landing page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -3,30 +3,28 @@
 @section('content')
 <div class="hero">
     <div class="container">
-        <div class="row align-items-center">
-            <div class="col hero-text">
-                <h1 class="mb-3">Galeri Inovasi Masyarakat (GAS-SIKAT)</h1>
+        <div class="row row-cols-md-2 row-cols-1 align-items-center">
+            <div class="col hero-text order-md-1 order-2">
+                <h1 class="mb-3 text-md-start">Galeri Inovasi Masyarakat (GAS-SIKAT)</h1>
                 <p class="mb-4">
                     <span>GAS-SIKAT</span> adalah aplikasi untuk mempublikasikan
                     dan mempromosikan berbagai inovasi yang telah
                     dihasilkan oleh masyarakat dalam berbagai bidang
                     kehidupan
                 </p>
-                <button class="btn btn-primary">Selengkapnya</button>
+                <button class="btn btn-primary" onclick="window.location.href='/inovasi'">Selengkapnya</button>
             </div>
-            <div class="col d-flex justify-content-center hero-image">
-                <img src="{{ asset('images/innovation.svg') }}" alt="">
-            </div>
+            <img class="col-md col-9 mx-auto hero-image order-md-2 order-1" src="{{ asset('images/innovation.svg') }}" alt="">
         </div>
     </div>
 </div>
 
-<div class="features container">
+<div class="features container pb-5">
     <h2 class="title mt-5">Fitur-Fitur GAS-SIKAT</h2>
     <div class="column gap-5 mt-5">
-        <div class="row align-items-center mb-4 feature">
-            <img class="col-4" src="{{ asset('images/innovation.svg') }}" alt="">
-            <div class="col feature-text">
+        <div class="row row-cols-sm-2 row-cols-1 g-3 align-items-center mb-4 feature">
+            <img class="col-sm-4 col-8 mx-auto" src="{{ asset('images/innovation.svg') }}" alt="">
+            <div class="col-sm-8 col feature-text">
                 <h3>Nama fitur pertama</h3>
                 <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis,
@@ -36,8 +34,8 @@
                 </p>
             </div>
         </div>
-        <div class="row align-items-center mb-4 feature">
-            <div class="col feature-text">
+        <div class="row row-cols-sm-2 row-cols-1 g-3 align-items-center mb-4 feature">
+            <div class="col-sm-8 col feature-text order-sm-1 order-2">
                 <h3>Nama fitur kedua</h3>
                 <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis,
@@ -46,11 +44,11 @@
                     cumque dolore fuga accusantium, ex ullam! Explicabo.
                 </p>
             </div>
-            <img class="col-4" src="{{ asset('images/innovation.svg') }}" alt="">
+            <img class="col-sm-4 col-8 mx-auto order-sm-2 order-1" src="{{ asset('images/innovation.svg') }}" alt="">
         </div>
-        <div class="row align-items-center mb-5 feature">
-            <img class="col-4" src="{{ asset('images/innovation.svg') }}" alt="">
-            <div class="col feature-text">
+        <div class="row row-cols-sm-2 row-cols-1 g-3 align-items-center mb-4 feature">
+            <img class="col-sm-4 col-8 mx-auto" src="{{ asset('images/innovation.svg') }}" alt="">
+            <div class="col-sm-8 col feature-text">
                 <h3>Nama fitur ketiga</h3>
                 <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis,
@@ -128,7 +126,7 @@
             </div>
         </div>
         <div class="text-center">
-            <button class="btn btn-primary">Lihat Semua Inovasi</button>
+            <button class="btn btn-primary" onclick="window.location.href='/inovasi'">Lihat Semua Inovasi</button>
         </div>
     </div>
 </div>
