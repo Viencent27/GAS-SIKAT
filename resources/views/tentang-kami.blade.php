07e6ffcce833fe0045f7b2380ae0e4ecb50142fd
yandi <lintangyandinugraha@gmail.com>
2023-11-23 17:47:17 +0700

    Implement front-end for About Us page

--- resources/views/tentang-kami.blade.php
+++ resources/views/tentang-kami.blade.php
@@ -1,5 +1,75 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>Ini halaman tentang kami</h1>
+<div class="container tentang-kami">
+    <h2 class="title mt-5 mb-5">Tim Kami</h2>
+    <div class="d-flex justify-content-center text-center">
+        <p>
+            Tim Capstone Dicoding C523-PR088 dengan Tema Teknologi
+            dalam Berbagai Aspek Kehidupan dan Judul Proyek Galeri
+            Inovasi Masyarakat (GAS-SIKAT)
+        </p>
+    </div>
+    <div class="row row-cols-lg-3 row-cols-sm-2 row-cols-1 justify-content-center g-5 mt-1 mb-5">
+        <div class="col">
+            <div class="card h-100 shadow-sm team-member text-center">
+                <div class="position-relative overflow-hidden">
+                    <img src="https://images.unsplash.com/photo-1585846416120-3a7354ed7d39?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Anatasya Lingkanwene Ering</h5>
+                    <p class="card-text text-body-secondary">UI/UX</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card h-100 shadow-sm team-member text-center">
+                <div class="position-relative overflow-hidden">
+                    <img src="https://images.unsplash.com/photo-1585846416120-3a7354ed7d39?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Lintang Yandi Nugraha</h5>
+                    <p class="card-text text-body-secondary">Front-End Developer</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card h-100 shadow-sm team-member text-center">
+                <div class="position-relative overflow-hidden">
+                    <img src="https://images.unsplash.com/photo-1585846416120-3a7354ed7d39?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Sukron Hadi Prasojo</h5>
+                    <p class="card-text text-body-secondary">Front-End Developer</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card h-100 shadow-sm team-member text-center">
+                <div class="position-relative overflow-hidden">
+                    <img src="https://images.unsplash.com/photo-1585846416120-3a7354ed7d39?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Dhimas Jatiwibowo</h5>
+                    <p class="card-text text-body-secondary">Back-End Developer</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card h-100 shadow-sm team-member text-center">
+                <div class="position-relative overflow-hidden">
+                    <img src="https://images.unsplash.com/photo-1585846416120-3a7354ed7d39?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Viencent</h5>
+                    <p class="card-text text-body-secondary">Back-End Developer</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
