52f3652234320418caf41f003303c615d9e2902f
yandi-nugraha <lintangyandinugraha@gmail.com>
2023-12-16 16:31:14 +0700

    Fix signInButton and signUpButton javascript code

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,17 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const containerLogin = document.getElementById('container-login');
 
-signUpButton.addEventListener('click', () => {
-	containerLogin.classList.add("right-panel-active");
-});
+if (signUpButton) {
+	signUpButton.addEventListener('click', () => {
+		containerLogin.classList.add("right-panel-active");
+	});
+}
 
-signInButton.addEventListener('click', () => {
-	containerLogin.classList.remove("right-panel-active");
-});
+if (signInButton) {
+	signInButton.addEventListener('click', () => {
+		containerLogin.classList.remove("right-panel-active");
+	});
+}
 
 function togglePassword(formType) {
 	var passwordInput = document.getElementById('password' + formType);
