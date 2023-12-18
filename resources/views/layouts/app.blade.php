baabd559cd8cd857646ee470100f7c3df39f4912
yandi-nugraha <lintangyandinugraha@gmail.com>
2023-12-18 12:19:17 +0700

    Add Dicoding tag to the header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Galeri Inovasi Masyarakat</title>
 
+    <meta name="dicoding:email" content="lintang.yandi.nugraha@mail.ugm.ac.id">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
